f9a1e877da4117c56697bc053fcc0f8ade7fd497
yulao <xsrkt26@outlook.com>
2023-12-09 15:18:40 +0800

    LinearRegressionModel0.1V
    计算逻辑完成，输出形式未确定，测试未完成

--- ScientificCalculator/src/model/LinearRegressionModel.java
+++ ScientificCalculator/src/model/LinearRegressionModel.java
@@ -1,32 +1,123 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * @author ：kiyotaka
  * @description：TODO
  * @date ：2023/11/28 17:41
  */
 public class LinearRegressionModel extends CalculatorModel{
-    private String argument;
-    private String dependent;
+    // inputExpression 参数包含线性回归的自变量X与因变量Y， 中间以 | 分开；
+
+    private ArrayList<Double> xList;
+    private ArrayList<Double> yList;
+
+    // 系数长度
+    private int coefficientLength;
+
+    // x * y
+    private ArrayList<Double> xyList;
+    // x ^ 2
+    private ArrayList<Double> xSquareList;
+    // y ^ 2
+    private ArrayList<Double> ySquareList;
+
+    // x 平均数
+    private Double xAverage;
+    // y 平均数
+    private Double yAverage;
+    // x * y 平均数；
+    private Double xyAverage;
+
+    // x 平均数的平方
+    private Double xAverageSquare;
+    // y 平均数的平方
+    private Double yAverageSquare;
+
+    // x 平方的平均数
+    private Double xSquareAverage;
+    // y 平方的平均数
+    private Double ySquareAverage;
+
+    // B 回归系数
+    private Double regressionCoefficientB;
+    // A 回归系数
+    private Double regressionCoefficientA;
+
+    // R 相关系数
+    private Double correlationCoefficientR;
 
     /**
      * @author: kiyotaka
-     * @description: get inputExpression,and spilt it into two pieces by |;
+     * @description: get inputExpression;
      * @date: 2023/12/3 11:06
      */
     public LinearRegressionModel(String inputExpression){
         this.inputExpression = inputExpression;
     }
 
+    /**
+     * @author: kiyotake
+     * @description: 输入发生变化时，更新输入数据
+     * @date: 2023/12/9 14:47
+     * @return
+     */
+    public void upDateInputExpression(String inputExpression) {
+        this.inputExpression = inputExpression;
+    }
+
     @Override
     /**
      * @author: kiyotaka
-     * @description: TODO
+     * @description: 综合逻辑：1.
      * @date: 2023/12/3 13:00
      * @return void
      */
     public void count() {
+        splitInputExpression();
+        countData();
+        transDataIntoString();
+    }
 
+    private void transDataIntoString() {
+
+    }
+
+    /**
+     * @author: kiyotaka
+     * @description: 计算数据
+     * @date: 2023/12/9 14:57
+     * @return
+     */
+    private void countData() {
+        coefficientLength = xList.size();
+        double xSum = 0, ySum = 0, xSquareSum = 0, ySquareSum = 0, xySum = 0;
+        for (int i = 0; i < coefficientLength; i++) {
+            double x = xList.get(i);
+            double y = yList.get(i);
+            xyList.add(x * y);
+            xSquareList.add(x * x);
+            ySquareList.add(y * y);
+
+            xSum += x;
+            ySum += y;
+            xySum += xyList.get(i);
+            xSquareSum += xSquareList.get(i);
+            ySquareSum += ySquareList.get(i);
+        }
+        xAverage = xSum / coefficientLength;
+        yAverage = ySum / coefficientLength;
+        xyAverage = xySum / coefficientLength;
+        xSquareAverage = xSquareSum / coefficientLength;
+        ySquareAverage = ySquareSum / coefficientLength;
+        xAverageSquare = xAverage * xAverage;
+        yAverageSquare = yAverage * yAverage;
+
+        regressionCoefficientB = (xAverage * yAverage - xyAverage) / (xAverageSquare - xSquareAverage);
+        regressionCoefficientA = yAverage - regressionCoefficientB * xAverage;
+        correlationCoefficientR = (xyAverage - xAverage * yAverage) /
+                Math.sqrt((xSquareAverage - xAverageSquare) * (ySquareAverage - yAverageSquare));
     }
 
     @Override
@@ -34,12 +125,37 @@ public class LinearRegressionModel extends CalculatorModel{
         return outputAnswer;
     }
 
+
+    /**
+     * @author: kiyotaka
+     * @description: 当自变量的数量与因变量的数量不一致时，输入不合法
+     * @date: 2023/12/9 13:10
+     * @return
+     */
     @Override
     public boolean checkIllegal() {
-        return false;
+        return xList.size() == yList.size();
     }
 
-    public void splitInputExpression() {
 
+    /**
+     * @author: kiyotaka
+     * @description: 把输入字符串转化为 XY两个字符串，然后分割转化为数字存储在xList ， yList 中
+     * @date: 2023/12/9 13:03
+     * @return
+     */
+    private void splitInputExpression() {
+        String[] temp = inputExpression.split("\\|");
+        String[] tempX = temp[0].split("\\s+");
+        String[] tempY = temp[1].split("\\s+");
+        for (String numberX: tempX) {
+            double value = Double.parseDouble(numberX);
+            xList.add(value);
+        }
+        for (String numberY: tempY) {
+            double value = Double.parseDouble(numberY);
+            yList.add(value);
+        }
+        coefficientLength = xList.size();
     }
 }
